43d726c00760c429d58957b65327a11e8e64d726
Steve <34704346+denomous@users.noreply.github.com>
2021-01-16 11:24:59 +0800

    Add files via upload

---
+++ InitPara_ForceControl.m
@@ -0,0 +1,17 @@
+clear;
+
+robot.body.length = 0.8;
+robot.body.width = 0.4;
+robot.body.thickness = 0.2;
+
+robot.body.weight = 50;
+
+robot.hipLegLength = 0;
+robot.upperLegLength = 0.4;
+robot.lowerLegLength = 0.4;
+
+robot.upperLegWeight = 0.5;
+robot.lowerLegWeight = 0.5;
+
+robot.legPosKp = 1000;
+robot.legPosKd = 100;
